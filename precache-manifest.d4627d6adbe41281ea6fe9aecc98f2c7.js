b2956261d1c1393f14a7ca596b10e8f53d7f9f14
SinhalaWebScience <96146@fhss.sjp.ac.lk>
2022-02-24 07:33:32 +0530

    Updates

---
+++ precache-manifest.d4627d6adbe41281ea6fe9aecc98f2c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c660a4e3ce55f870a1dfc73dae8903e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc6b76af84a834813b0",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "108663e32467404a5d67",
+    "url": "/static/css/main.ced2ec41.chunk.css"
+  },
+  {
+    "revision": "4cc6b76af84a834813b0",
+    "url": "/static/js/2.88c7a829.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.88c7a829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108663e32467404a5d67",
+    "url": "/static/js/main.8a4635fe.chunk.js"
+  },
+  {
+    "revision": "f7bba484c3683fdbcb0e",
+    "url": "/static/js/runtime-main.07ddf7ff.js"
+  },
+  {
+    "revision": "675832e2a635b59cdd8988bed51d0e2a",
+    "url": "/static/media/android.675832e2.png"
+  },
+  {
+    "revision": "2e77a981636a19affd7e375dcdc5e0ca",
+    "url": "/static/media/ios.2e77a981.jpg"
+  },
+  {
+    "revision": "b3c59c773de2b0984ff9014f21c0ae34",
+    "url": "/static/media/pic.b3c59c77.jpg"
+  },
+  {
+    "revision": "bc2f64ff230cbf4b8f6093c574985022",
+    "url": "/static/media/web.bc2f64ff.jpg"
+  }
+]);
